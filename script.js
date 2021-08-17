d6c451dc25b7953b87b5a073231ae655eb22150d
Presenter <hendrik.wermser@ironhack.com>
2021-08-17 18:57:22 +0200

    restyled the character output using break

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ document.querySelector('#refreshbutton').onclick = () => {
       respArr.forEach(character => {
 
         document.querySelector('#characters-list').innerHTML += `<li class="list-group-item">
-          ID: ${character.id} - ${character.name}          
+          ID: <span>${character.id}</span>
+          <br>
+          NAME: <span>${character.name}</span>       
       </li>`
 
       });
